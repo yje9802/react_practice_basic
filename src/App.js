49c2566b3fe2713a92b69dd2f0fbefaa84b9f1ee
Ji-eun Yang <jidak1165@gmail.com>
2023-03-09 00:02:38 +0900

    Practice (Crypto List)
    API 활용하여 암호화폐 리스트 생성

--- src/App.js
+++ src/App.js
@@ -2,51 +2,30 @@
 // import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Search() {
-	const [keyword, setKeyword] = useState("");
-	const onChange = (event) => setKeyword(event.target.value);
-	// this will be rendered only when the keyword is changed
-	useEffect(() => {
-		if (keyword !== "" && keyword.length > 5) {
-			console.log("Search for", keyword);
-		}
-	}, [keyword]);
-
-	return (
-		<input
-			value={keyword}
-			type="text"
-			placeholder="Search here..."
-			onChange={onChange}
-		/>
-	);
-}
-
 function App() {
-	const [counter, setValue] = useState(0);
-	const [showing, setShowing] = useState(false);
-	const onClick = () => setValue((prev) => prev + 1);
-	const onShowClick = () => setShowing((prev) => !prev);
-
-	const iRunOnlyOnce = () => {
-		console.log("i run only once");
-	};
-	// iRunOnlyOnce will be rendered only once at the very beginning
-	useEffect(iRunOnlyOnce, []);
-
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
 	useEffect(() => {
-		if (showing === true) {
-			console.log("created");
-			return () => console.log("destroyed");
-		}
-	}, [showing]);
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((response) => response.json())
+			.then((json) => {
+				setCoins(json);
+				setLoading(false);
+			});
+	}, []);
 
 	return (
 		<div>
-			<h1>{counter}</h1>
-			<button onClick={onClick}>Click Me</button>
-			<button onClick={onShowClick}>{showing ? "Hide" : "Show"}</button>
-			{showing ? <Search /> : null}
+			<h1>The Coins</h1>
+			{loading ? <h2>Loading...</h2> : <h2>({coins.length})</h2>}
+			<ul>
+				{coins.map((coin) => (
+					<li>
+						{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}{" "}
+						USD
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
