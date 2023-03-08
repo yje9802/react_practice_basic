54de658a21a853a0aec5cd3e9b1d9bac3efc7135
Ji-eun Yang <jidak1165@gmail.com>
2023-03-08 22:58:20 +0900

    first practice
    노마드 코더
    ReactJS로 영화 웹 서비스 만들기
    EFFECTS 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Search() {
+	const [keyword, setKeyword] = useState("");
+	const onChange = (event) => setKeyword(event.target.value);
+	// this will be rendered only when the keyword is changed
+	useEffect(() => {
+		if (keyword !== "" && keyword.length > 5) {
+			console.log("Search for", keyword);
+		}
+	}, [keyword]);
+
+	return (
+		<input
+			value={keyword}
+			type="text"
+			placeholder="Search here..."
+			onChange={onChange}
+		/>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setValue] = useState(0);
+	const [showing, setShowing] = useState(false);
+	const onClick = () => setValue((prev) => prev + 1);
+	const onShowClick = () => setShowing((prev) => !prev);
+
+	const iRunOnlyOnce = () => {
+		console.log("i run only once");
+	};
+	// iRunOnlyOnce will be rendered only once at the very beginning
+	useEffect(iRunOnlyOnce, []);
+
+	useEffect(() => {
+		if (showing === true) {
+			console.log("created");
+			return () => console.log("destroyed");
+		}
+	}, [showing]);
+
+	return (
+		<div>
+			<h1>{counter}</h1>
+			<button onClick={onClick}>Click Me</button>
+			<button onClick={onShowClick}>{showing ? "Hide" : "Show"}</button>
+			{showing ? <Search /> : null}
+		</div>
+	);
 }
 
 export default App;
